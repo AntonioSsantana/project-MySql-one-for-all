056237e8cd64855c9e7c1f238dc85e784889c090
Antônio Santana <antonionetosg6@gmail.com>
2022-12-06 03:29:17 -0300

    feat: requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+SELECT
+   COUNT(DISTINCT cancoes.id) AS cancoes,
+   COUNT(DISTINCT artistas.id) AS artistas,
+   COUNT(DISTINCT albuns.id) AS albuns
+FROM SpotifyClone.cancoes AS cancoes
+INNER JOIN SpotifyClone.artistas AS artistas
+INNER JOIN SpotifyClone.albuns AS albuns
