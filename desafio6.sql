c4151e3400387adff638764c5397e4fd498d821b
Antônio Santana <antonionetosg6@gmail.com>
2022-12-14 07:32:50 -0300

    feat: requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT
+	MIN(plans.valor) AS faturamento_minimo,
+    ROUND(AVG (plans.valor), 2) AS faturamento_medio,
+    MAX(plans.valor) AS faturamento_maximo,
+    SUM(plans.valor) AS faturamento_total
+FROM SpotifyClone.planos AS plans
+INNER JOIN SpotifyClone.usuarios AS users
+ON plans.id = users.plano_id
